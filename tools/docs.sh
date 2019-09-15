3ab5bdd5a38386b5e24a24f2299172cd83baa2e1
Chris Martin <ch.martin@gmail.com>
2019-09-14 22:06:51 -0600

    linked doc source

--- tools/docs.sh
+++ tools/docs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nix-shell ./tools/shell.nix --run 'haddock caboodle.hs --odir docs --html --pretty-html'
+nix-shell ./tools/shell.nix --run 'haddock caboodle.hs --odir docs --html --pretty-html --hyperlinked-source'
